8f8b43d5fddba9a903d47618819e1132dea53b3f
0354ff <maristepykovac@gmail.com>
2025-06-29 13:41:40 +0300

    meow
    можно посмотреть на прикольные кривые и потягать за ползунки

---
+++ meow.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+st.title("🦋 мейд бай кувшинка")
+
+
+k = st.slider(
+    "max значение тешки: t = k·π",    
+    min_value=1, max_value=20,     
+    value=12                       
+)
+
+num_points = st.slider(
+    "Количество точек (точность)",
+    min_value=500, max_value=3000,
+    value=1000, step=100
+)
+
+color = st.selectbox(
+    "Цвет кривой",
+    options=["purple", "blue", "red", "green", "black", "orange", "pink",],
+    index=0
+) 
+
+sin_power = st.slider("Степень синуса (sin(t/12)^n)", min_value=1, max_value=50, value=5)
+
+
+t = np.linspace(0, k * np.pi, num_points)
+
+expression = np.exp(np.cos(t)) - 2 * np.cos(4*t) + np.sin(t/12)**sin_power
+x = np.cos(t) * expression
+y = np.sin(t) * expression 
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.plot(y, x, color = color)
+ax.set_title("Кривая бабочки")
+ax.axis('equal')
+ax.grid(True)
+
+st.pyplot(fig)
